17f738668dd677683d0811996fbd6cd52f316119
Gerarcore <grv1405@gmail.com>
2026-05-19 17:12:24 -0500

    actualizar menu de platos con la api

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-def main():
-  print("Hello learners!")
+import requests
+# Traer la lista de platos tipicos de Colombia
 
+def dish_fetch(num):
+
+    url = "https://api-colombia.com/api/v1/TypicalDish"
+    response = requests.get(url)
+    dishes = response.json()
+    dish = dishes[num]
+    dish_info = {
+        "name": dish["name"],
+        "description": dish["description"]
+    }
+    return dish_info
+
+# Seleccionar un plato del menú:
+def main():
+  numero = int(input("Ingresa el número de la lista de platos:"))
+  dish =dish_fetch(numero)
+  print (dish)
+
+#Resultado final
 if __name__=="__main__":
   main()
